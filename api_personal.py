0e1a851dd740fe3cc0666a1eedfc22ed2f50fbd7
AGD-2022 <117851653+AGD-2022@users.noreply.github.com>
2023-05-11 19:21:56 +0200

    Update api_personal.py

--- api_personal.py
+++ api_personal.py
@@ -4,36 +4,25 @@ from bs4 import BeautifulSoup
 
 r = requests.get('https://www.dailysmarty.com/topics/python')
 
-print(r.text)
 
 datos = BeautifulSoup(r.text, 'html.parser')
 
-print(datos)
+links_all = datos.find_all(['a'])
 
-#links_all = datos.find_all(['a'])
+post_links = []
 
-#print(datos.find_all('a'))
+for link in links_all:
+    href = link.get('href')
+    if href and href.startswith('/post'):
+        href2 = inflection.titleize(href)
+        post_links.append(href2)
 
-#links_clean = []
 
-#links_clean = links_all['href']
+links_clean = [item[7:] for item in post_links]
 
-#for link in links_all:
-#    if "p" in link:
-#        links_clean.append(link)
-#        print(f'se agrego '+ link)
-#
+lista_final = links_clean[1:]
 
-#print(links_clean)
+for x in lista_final:
+    print(x)
 
-#post_links = [link for link in links_clean['href'] if link.startswith('/pos')]
-
-#print(inflection.humanize(r.text))
-
-#print(BeautifulSoup(r.text))
-
-#print(BeautifulSoup(inflection.humanize(r.text)))
-
-#print(post_links)
-
-#print(links_clean)
+print("fin de la ejecucion")
